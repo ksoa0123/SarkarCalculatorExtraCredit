11c82975fe68d30dcb64ecac41a812b7d4f98fa7
unknown <BT_3W2_05@BKTECH.LOCAL>
2022-11-01 11:21:35 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        //Main method
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your expression: ");
+
+        String userExpression = scan.nextLine();
+
+        //Find first, second, operation in the expression in format. Ex: 100 * 923
+
+        
+    }
+}
