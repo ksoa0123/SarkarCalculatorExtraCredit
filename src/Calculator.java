180c3e3ccec6b05f43f430e2602c5821cbbf35f5
kosausrk <kosausrk@gmail.com>
2022-11-02 21:40:59 -0400

    Working parse() and multiply()

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,45 @@
 public class Calculator {
+    public double firstDigit, secondDigit;
+    public String expression;
+
+    public Calculator(double firstDigit, double secondDigit, String expression){
+        this.firstDigit = firstDigit;
+        this.secondDigit = secondDigit;
+        this.expression = expression;
+    }
+
+
+    //Methods Divide, Multiply, Subtract, Exponent, Addition, Mod
+    public double divide(){
+        return firstDigit / secondDigit;
+    }
+
+    public double multiply(){
+        return firstDigit * secondDigit;
+    }
+
+    public double subtract(){
+        return firstDigit - secondDigit;
+    }
+
+    public double exponent(){
+        return Math.pow(firstDigit, secondDigit);
+    }
+
+    public double addition(){
+        return firstDigit + secondDigit;
+    }
+
+    public double mod(){
+        return firstDigit % secondDigit;
+    }
+
+
+    //Method run which runs based on the expression
+    public void calculate(){
+        if (expression.equals("*")){
+            System.out.println(multiply());
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,17 @@ public class Main {
 
         //Find first, second, operation in the expression in format. Ex: 100 * 923
 
-        
+        String firstDigitString  = userExpression.substring(0, userExpression.indexOf(" ")); //First digit
+        String secondDigitString = userExpression.substring(userExpression.indexOf(" ") + 3); //Second Digit
+        String expression = userExpression.substring(userExpression.indexOf(" ") + 1, userExpression.indexOf(" ") + 2 );
+
+        double firstDigit = Double.parseDouble(firstDigitString);
+        double secondDigit = Double.parseDouble(secondDigitString);
+
+
+        Calculator exp1 = new Calculator(firstDigit, secondDigit, expression);
+
+        exp1.calculate();
+
     }
 }
